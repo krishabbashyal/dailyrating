82cce9371089c9552a1e9dbd98ca03a4a80040d9
Krishab Bashyal <krishab2002@gmail.com>
2026-08-18 13:37:41 -0500

    deployment fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
-import { useState } from 'react'
+// import { useState } from 'react'
 import EmojiSlider from './components/EmojiSlider'
 import GreetingBar from './components/GreetingBar'
 import CustomButton from './components/CustomButton'
 
 const App = () => {
 
-  const [rating, setRating] = useState(0)
+  //const [rating, setRating] = useState(0)
 
   return (
     <main>
@@ -15,7 +15,7 @@ const App = () => {
         <EmojiSlider/>
       </div>
       <div className="flex flex-row items-center justify-center mt-12">
-        <CustomButton customClasses="bg-brand-primary w-full mx-8 h-12" label="Submit" onClick={() => console.log(rating)}/>
+        <CustomButton customClasses="bg-brand-primary w-full mx-8 h-12" label="Submit" onClick={() => {}}/>
       </div>
     </main>
   )
